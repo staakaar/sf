35595d673857702bffe60184fc3d7d821994043a
otobakataro <vancyroletaka@gmail.com>
2023-03-19 23:46:41 +0900

    install zx_deno

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
-export function add(a: number, b: number): number {
-  return a + b;
+// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+
+import {$} from 'https://deno.land/x/zx_deno/mod.mjs'
+await $`date`
+
+if (import.meta.main) {
+  const git_log = Deno.run({
+    cmd: ["git", "branch", "--all", "--format='%(refname:short)'"],
+    stdin: "piped",
+    stdout: "piped",
+  })
+
+  let branch_word: string | null = window.prompt("ブランチを入力してください。");
+
+  if (!branch_word) branch_word = ""
+  const input = new TextEncoder().encode(branch_word)
+
+  await git_log.stdin().write(input);
+
+  const result = await git_log.output();
+  const text = new TextDecoder().decode(result);
+  console.log(text)
+  // let response = await exec(`zsh -c "fzf | xargs git checkout"`);
+
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+/** MEMO
+    zx_deno install
+    Gitでコミットを一つにまとめてpushするまでを自動化するコマンドを作成する。
+    1. git checkout dev-master <= チェックアウトするブランチをGUIで選択移動できるようにする。
+    2. git pull
+    3. git checkout 作業ブランチ
+    4. git log --oneline でコミットIDを取得
+    5. 4で取得したコミットIDを git rebase -i コミットIDとする
+    6. エディターで編集
+    7. 実行後に git push --force-with-lease 作業ブランチ
+*/
